37eb33cbb1bc7efa98f950e238967313a2578b8b
Shahbaz Singh <shahbaz.singh@proton.me>
2026-06-28 13:00:38 -0300

    enable jit transformer

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,4 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 
 const databaseUrl = process.env.DATABASE_URL ?? "file:./local.db";
 
-export const db = drizzle(databaseUrl);
+export const db = drizzle(databaseUrl, { jit: true });
